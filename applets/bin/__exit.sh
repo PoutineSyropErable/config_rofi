3709d6d69c4979e94d0ee7cc5d09250c0018a5e4
François Pagé <francois.page@mail.mcgill.ca>
2025-04-21 18:26:42 -0400

    small upkeep

--- applets/bin/__exit.sh
+++ applets/bin/__exit.sh
@@ -5,6 +5,7 @@ if pgrep -x "Hyprland" >/dev/null; then
 	# Hyprland is running, exit Hyprland
 	#notify-send "Hyperland"
 	echo "Hyperland"
+	uwsm stop
 	hyprctl dispatch exit
 
 elif pgrep -x "i3" >/dev/null || pgrep -x "i3-with-shmlog" >/dev/null; then
